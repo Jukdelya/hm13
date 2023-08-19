d60c1e53e935faed3b0cf3bcff993f1434221b6e
Jukdelya <F43daniilred@gmail.com>
2023-08-19 15:40:59 +0300

    ee

---
+++ src/lesson6/If.java
@@ -0,0 +1,12 @@
+package lesson6;
+
+public class If {
+    public static void main(String[] args) {
+        int i =5;
+        if(i<10){
+            System.out.println("Правильно");
+        } else if (i>20) {
+            System.out.println("Неправильно");
+        }
+    }
+}
